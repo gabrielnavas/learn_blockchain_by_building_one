ec30a73ac7400b70eb0d1692c392165fd4b57758
Navas <gabrielnavas@email.com>
2021-06-03 01:14:23 -0300

    ConnectionPool class

--- chapter_7/funcoin/funcoin/connections.py
+++ chapter_7/funcoin/funcoin/connections.py
@@ -1,22 +1,32 @@
+import structlog
+from more_itertools import take
+
+logger = structlog.getLogger(__name__)
+
 class ConnectionPool:
     def __init__(self):
-        pass
+        self.connection_pool = dict()
 
     def broadcast(self, message):
-        # Method to broadcast a message to all connected peers
-        pass
+        for user in self.connection_pool:
+            user.write(f"{message}".encode())
     
     @staticmethod
     def get_address_string(writer):
-        # Get a peer's ip:port (address)
-        pass
+        ip = writer.address["ip"]
+        port = writer.address["port"]
+        return f"{ip}:{port}"
 
     def add_peer(self, writer):
-        pass
+        address = self.get_address_string(writer)
+        self.connection_pool[address] = writer
+        logger.info("Added new peer to pool", address=address)
 
     def remove_peer(self, writer):
-        pass
+        address = self.get_address_string(writer)
+        self.connection_pool.pop(address)
+        logger.info("Removed new peer to pool", address=address)
+
 
     def get_alive_peers(self, count):
-        # Return some connected peers
-        # pass   
+        return take(count, self.connection_pool.items())
