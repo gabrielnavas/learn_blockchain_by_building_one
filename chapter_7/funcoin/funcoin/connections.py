761277cb27be61571465060f9063d98014215584
Navas <gabrielnavas@email.com>
2021-06-03 00:17:59 -0300

    prototype of the ConnectionPool class

---
+++ chapter_7/funcoin/funcoin/connections.py
@@ -0,0 +1,22 @@
+class ConnectionPool:
+    def __init__(self):
+        pass
+
+    def broadcast(self, message):
+        # Method to broadcast a message to all connected peers
+        pass
+    
+    @staticmethod
+    def get_address_string(writer):
+        # Get a peer's ip:port (address)
+        pass
+
+    def add_peer(self, writer):
+        pass
+
+    def remove_peer(self, writer):
+        pass
+
+    def get_alive_peers(self, count):
+        # Return some connected peers
+        # pass   
